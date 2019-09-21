14254d3bdd019aea0f5c2ecf2ba6da27d5071c78
Inve <inveniahadouken@gmail.com>
2019-09-21 16:42:43 -0300

    created barchart

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
 import React, { Component } from "react";
-import pigData from "./wild-pig-data.json";
+import BarChart from './components/Chart/BarChart'
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-
-        <table>
-          <tbody>
-            <tr>
-              <th>Year</th>
-              <th>Island</th>
-              <th>Population</th>
-            </tr>
-            {pigData["PIG POPULATIONS"].map((datum, index) => (
-              <tr key={index}>
-                <td>{datum.year}</td>
-                <td>{datum.island}</td>
-                <td>{datum.pigPopulation}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+       <BarChart />
       </div>
     );
   }
