bccd1c191006fa6195e890d82321f8bcfd208b26
Inve <inveniahadouken@gmail.com>
2019-09-25 01:05:40 -0300

    mayor refactor & added material-ui components

--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -1,64 +1,78 @@
 import React, { Component } from 'react'
 import BarChart from '../BarChart/BarChart'
 import pigData from '../../wild-pig-data.json'
+import ProgressBar from '../ProgressBar/ProgressBar'
+import {validateYear, arrYearFiltered} from '../helpers'
 import * as queryString from 'query-string'
 import './styles/styles.css'
 
-let progressChart
+import Box from '@material-ui/core/Box';
+
+//constants
+const ADD_ONE_YEAR = 1
+
+
+let progressBarChart
 
 class Main extends Component {
     constructor(props){
         super(props)
+       
+        //retrieve data from json
+        this.data = pigData['PIG POPULATIONS']
+        const minYear = Math.min(...arrYearFiltered(this.data))
+        const maxYear = Math.max(...arrYearFiltered(this.data))
+
+        const howManyYears = arrYearFiltered.length
+
+        const progressPercent = (100/howManyYears) * 100
+
         this.state = {
-            selectedYear: '',
             years: [],
-            minYear: '',
-            maxYear: ''
+            minYear: minYear,
+            maxYear: maxYear,
+            progress: progressPercent
         }
-        console.log(queryString.parse(this.props.location.search))
+
+    }
+
+
+    UNSAFE_componentWillMount () {
+        //retrieve data from params
+        const {data} = this
+        const {minYear} = this.state
+        const {paused, year} = queryString.parse(this.props.location.search)
+        const isPaused = paused ? paused === 'true' : false
+        const yearsFiltered = arrYearFiltered(data)
+        this.setState({isPaused, selectedYear: validateYear(year, minYear), years: yearsFiltered})
     }
 
     componentDidMount() {
-        const yearList = pigData["PIG POPULATIONS"]
-                        .map(item => item.year)
-
-        const filterYearList = yearList.filter((year, index) => {
-           return yearList.indexOf(year) === index
-        })
-        const minYear = Math.min(...filterYearList)
-        const maxYear = Math.max(...filterYearList)
-
-        this.setState({
-            years: filterYearList, 
-            selectedYear: filterYearList[0], 
-            minYear: minYear, 
-            maxYear: maxYear
-        })
-
-}
-
-    UNSAFE_componentWillMount () {
-        const {paused, year} = queryString.parse(this.props.location.search)
-        const parseYear = parseInt(year, 10)
-        const isPaused = paused ? paused === 'true' : false
-
-        this.setState({isPaused, selectedYear: parseYear})
-        console.log(parseYear)
+    
+        const {isPaused} = this.state
+        if(isPaused === true) {
+          this.startGraphic()
+        } 
     }
+
+    startGraphic = () => progressBarChart = setInterval(() => {this.changeSelectedYear()}, 2000)
+
+    stopGraphic = () => clearInterval(progressBarChart)
 
     changeSelectedYear = () => {
-        const {selectedYear, maxYear, years} = this.state
-
+       const {selectedYear, years, minYear, maxYear} = this.state
+ 
        const indexSelectedYear = years.indexOf(selectedYear) 
 
-       selectedYear < maxYear ? this.setState({selectedYear: years[indexSelectedYear + 1]}) 
-                              : this.setState({selectedYear: years[0]})
+
+       if(selectedYear < maxYear) {
+           this.setState({selectedYear: years[indexSelectedYear + ADD_ONE_YEAR]}) 
+        } else {
+            this.setState({selectedYear: minYear, isPaused: false})
+            this.stopGraphic()
+        }
+                          
     }
-
-    startGraphic = () => progressChart = setInterval(() => {this.changeSelectedYear()}, 2000)
-
-    stopGraphic = () => clearInterval(progressChart)
-    
 
     onClickPaused = () => {
 
@@ -66,28 +80,35 @@ class Main extends Component {
      
         this.setState({isPaused: !isPaused}, () => {
             this.state.isPaused ? this.startGraphic() : this.stopGraphic()
-        })
-            
+        })    
         
    }    
 
-
     render(){
+        const {data} = this
         const { isPaused, selectedYear} = this.state
-        console.log(isPaused, selectedYear)
+        const yearsTitleToString = String(selectedYear)
         return (
-            <div className='main-container'>
-                <div className='title'>
-                    <h1>{selectedYear}</h1>
+            <div>
+            <Box display="flex" p={1} justifyContent="center">
+                <div className='main-container'>
+                    <div className='title'>
+                        <h1>{yearsTitleToString}</h1>
+                    </div>
+                <BarChart 
+                    {...this.props}
+                    data={data} 
+                    paused={isPaused}
+                    year={selectedYear}
+                />
+                <ProgressBar className='progress-bar' variant='determinate' />
+                <div className='playPause-container'>
+                    <button onClick={this.onClickPaused}>{!isPaused ? 'Play' : 'Pause'}</button>
                 </div>
-            <BarChart 
-            data={pigData["PIG POPULATIONS"]} 
-            paused={isPaused}
-            year={selectedYear}
-            {...this.props}/>
-            <div className='playPause-container'>
-                <button onClick={this.onClickPaused}>{!isPaused ? 'Play' : 'Pause'}</button>
-            </div>
+           
+                </div>
+            </Box>
+                
             </div>
         )
     }
