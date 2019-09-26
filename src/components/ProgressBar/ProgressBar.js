b5a1c488ac6c41a667c653cbf7e1c10068276e5c
Inve <inveniahadouken@gmail.com>
2019-09-26 18:22:44 -0300

    added styles

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,27 +1,30 @@
-import React from 'react'
-import { withStyles } from '@material-ui/core/styles'
-import LinearProgress from '@material-ui/core/LinearProgress';
-//import { mergeClasses } from '@material-ui/styles';
+import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import { LinearProgress } from '@material-ui/core';
 
 const styles = {
-    bar: {
-      height: 25,
-      width: '100%'
-    }
+  root: {
+    display: 'flex',
+    alignItems: 'center',
+    flexGrow: 1,
+    height: 8,
+    borderRadius: '20px',
+    margin: '10px',
+  },
+  colorPrimary: {
+    background: 'linear-gradient(45deg, #aba9bb 30%, #aba9bb 90%)'
   }
+};
 
-class ProgressBar extends React.Component {
-    render(){
-        const {progress} = this.props
-        return (
-        <LinearProgress
-            color="primary"
-            value={progress}
-            variant="determinate"
-            className={styles.bar}
-         />
+class ColoredLinearProgress extends Component {
+  render() {
+    const { classes, progress } = this.props;
+    return( 
+      <div className={classes.root}>
+    <LinearProgress value={progress} variant='determinate' color='primary' {...this.props} clasName={classes.colorPrimary}/> 
+    </div>
     )
-    }
+  }
 }
 
-export default withStyles(styles)(ProgressBar)
+export default  withStyles(styles)(ColoredLinearProgress);
